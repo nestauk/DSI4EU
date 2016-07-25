1e88a8a38280b7ab3fa1a894b0d0fb52292836d0
Alex Pandele <alecs@inoveb.co.uk>
2016-07-25 12:51:09 +0100

    update footer

--- www/footer.php
+++ www/footer.php
@@ -1,9 +1,126 @@
+<?php
+use \DSI\Service\URL;
+
+?>
 </div>
+<div class="footer">
+    <div class="container-wide">
+        <div class="newsletter-block" data-ix="fadeinuponload-3">
+            <div class="w-row">
+                <div class="w-col w-col-6">
+                    <h2 class="newsletter-signup-h2">Stay up to date with the DSI4EU newsletter</h2>
+                </div>
+                <div class="w-col w-col-6">
+                    <div class="w-form">
+                        <form class="w-clearfix" data-name="Email Form" id="email-form" name="email-form">
+                            <input class="newsletter-signup-input w-input" data-name="Email" id="email" maxlength="256"
+                                   name="email" placeholder="Enter your email address" required="required" type="email">
+                            <input class="newsletter-signup-submit w-button" data-wait="Please wait..." type="submit"
+                                   value="Submit">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-columns w-row">
+            <div class="w-col w-col-4">
+                <img class="footer-brand" src="<?php echo SITE_RELATIVE_PATH ?>/images/all white.svg">
+            </div>
+            <div class="w-col w-col-2">
+                <h3 class="footer-h3">People</h3>
+                <ul class="w-list-unstyled">
+                    <li class="footer-link">
+                        <a class="footer-link" href="<?php echo URL::dashboard() ?>">Your dashboard</a>
+                    </li>
+                    <li class="footer-link">
+                        <a class="footer-link" href="#">Who uses DSI4EU?</a>
+                    </li>
+                    <li class="footer-link">
+                        <a class="footer-link" href="#">Join DSI4EU</a>
+                    </li>
+                    <li class="footer-link">
+                        <a class="footer-link" href="#">Help centre</a>
+                    </li>
+                    <li class="footer-link">
+                        <a class="footer-link" href="#">Report abuse</a>
+                    </li>
+                    <li class="footer-link">
+                        <a class="footer-link" href="#">Terms of service</a>
+                    </li>
+                    <li class="footer-link">
+                        <a class="footer-link" href="#">Privacy policy</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="w-col w-col-2">
+                <h3 class="footer-h3">Projects</h3>
+                <ul class="w-list-unstyled">
+                    <li class="footer-link">
+                        <a class="footer-link" href="#">Add a project to DSI4EU</a>
+                    </li>
+                    <li class="footer-link">
+                        <a class="footer-link" href="#">Report a&nbsp;project</a>
+                    </li>
+                    <li class="footer-link">
+                        <a class="footer-link" href="#">View project listing</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="w-col w-col-2">
+                <h3 class="footer-h3">Organisations</h3>
+                <ul class="w-list-unstyled">
+                    <li class="footer-link">
+                        <a class="footer-link" href="#">Add an organisation</a>
+                    </li>
+                    <li class="footer-link">
+                        <a class="footer-link" href="#">Report an organisation</a>
+                    </li>
+                    <li class="footer-link">
+                        <a class="footer-link" href="#">View partner organisations</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="w-col w-col-2">
+                <h3 class="footer-h3">Development</h3>
+                <a class="footer-link" href="<?php echo URL::feedback() ?>">Feedback</a>
+            </div>
+        </div>
+        <div class="footer-partner-row w-row">
+            <div class="w-clearfix w-col w-col-6">
+                <div class="footer-small-print">Nesta is a registered charity in England and Wales 1144091 and Scotland
+                    SC042833. Our main address is 1 Plough Place, London, EC4A 1DE
+                </div>
+                <img class="footer-cc-img" src="<?php echo SITE_RELATIVE_PATH ?>/images/88x31.png">
+                <div class="footer-small-print">All our work is licensed under a&nbsp;
+                    <a target="_blank" class="footer-link-small"
+                       href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Creative
+                        Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>, unless it says
+                    otherwise.
+                </div>
+            </div>
+            <div class="w-clearfix w-col w-col-6">
+                <a class="footer-partner-link w-inline-block" href="#">
+                    <img class="footer-logo" src="<?php echo SITE_RELATIVE_PATH ?>/images/logo_SUPSI.png" width="125">
+                </a>
+                <a class="footer-partner-link w-inline-block" href="#">
+                    <img class="footer-logo"
+                         src="<?php echo SITE_RELATIVE_PATH ?>/images/waag-f1d052f43133268eaf2e13090a0b4bf1.png"
+                         width="125">
+                </a>
+                <a class="footer-partner-link w-inline-block" href="#">
+                    <img class="footer-logo"
+                         src="<?php echo SITE_RELATIVE_PATH ?>/images/nesta-6a9b5fe999e8323b379ccc0d8e70290f.png"
+                         width="125">
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php /*
 <div class="footer w-section">
     <div class="container-wide">
-
-        <?php // newsletter ?>
-
         <div class="footer-columns w-row">
             <div class="w-col w-col-4">
                 <img class="footer-brand" src="<?php echo SITE_RELATIVE_PATH ?>/images/all white.svg">
@@ -85,6 +202,8 @@
         </div>
     </div>
 </div>
+ */ ?>
+
 <div class="cookies" id="cookies">
     <div class="container-wide">
         <div class="w-row">
