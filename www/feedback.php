ba2e2b555355b731225d5b6c0f58e35d932ed7d7
Alex Pandele <alecs@inoveb.co.uk>
2016-07-27 15:34:08 +0100

    Feedback update

--- www/feedback.php
+++ www/feedback.php
@@ -2,10 +2,12 @@
 
     <div class="w-container feed-back-conteiner" ng-controller="FeedbackController">
         <div class="feed-back-collection">
-            <h1 class="feedback-h1">DSI4EU Feed back form</h1>
-            <br /><br />
-            <p class="feed-back-info">Thank you for agreeing to help us develop the DSI4EU platform by completing the
-                following tasks. Please could you forward any feedback you have on the following items:</p>
+            <h1 class="feedback-h1">DSI4EU Feedback form</h1>
+            <br/><br/>
+            <p class="feed-back-info">Digitalsocial.eu is a work in progress. We are keen to gather feedback from our
+                users. Please let us
+                know about anything you find on the site that doesn't work as you expect and any ideas you have on how
+                the site could be improved.</p>
             <div class="w-form feedback-form-wrapper">
                 <form class="w-clearfix" ng-submit="sendFeedbackSubmit()">
                     <input type="text" placeholder="Your name" class="w-input feedback-form-input"
