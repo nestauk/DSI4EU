6fd9808c8f6dc0af9a83d00d1362f96a12f530b4
Alex Pandele <alecs@inoveb.co.uk>
2016-07-28 10:38:49 +0100

    Privacy policy and Terms of use pages content

--- www/privacy-policy.php
+++ www/privacy-policy.php
@@ -15,13 +15,120 @@ $totalOrganisations = (new \DSI\Repository\OrganisationRepository())->countAll()
     <div class="w-section section-grey">
         <div class="container-wide">
             <div class="w-row">
-                <div class="w-col w-col-6 intro-col">
-                    <h2 class="home-h2">What is the purpose of DSI4EU?</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
-                </div>
-                <div class="w-col w-col-6 intro-col">
-                    <h2 class="home-h2">What do the data visualisations below show?</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+                <div class="w-col w-col-12 intro-col">
+                    <p>
+                        This website is managed by Nesta (“Nesta”) on behalf of the European Commission and our
+                        partners, Waag Society and SUPSI. This privacy policy explains how the European Commission,
+                        Nesta and our partners use personal information which we collect via this site and what cookies
+                        we use.
+                    </p>
+
+                    <h2 class="home-h2">1. What kind of information do we collect?</h2>
+                    <p>
+                        When you register or sign up on the site, we will ask you for personal information such as your
+                        name, email address, location and other personal details. If you register organisations and/or
+                        projects we will link those to your personal profile and display your name as a collaborator on
+                        that organisation/project profile. We may also ask you for feedback about digitalsocial.eu or to
+                        complete surveys. We may record your activity and preferences when visiting the sites (see
+                        "Cookies", below). If you post content or communicate via the site, we may also store and
+                        monitor your content and communications.
+                    </p>
+
+                    <h2 class="home-h2">2. What do we do with information we collect?</h2>
+                    <p>
+                        We will use your personal information to operate the site, to send you newsletters, publications
+                        and other information about digitalsocial.eu, our partners and activities. We may also use your
+                        information to carry out analysis and research to improve our publications, events and
+                        activities, to prevent and detect fraud and abuse, and to protect other users.
+                        Please make sure that any personal details you provide are accurate and up to date, and let us
+                        know about any changes. Please get consent first before giving us anyone else’s information.
+                    </p>
+
+                    <h2 class="home-h2">3. How to unsubscribe</h2>
+                    <p>
+                        If you no longer want to receive communications from us, please click the unsubscribe link on
+                        any email from us or, where available, change the preferences on your account. If you want to
+                        remove
+                        your personal information from digitalsocial.eu please email contact@digitalsocial.eu.
+                    </p>
+
+                    <h2 class="home-h2">4. Who else has access to your information?</h2>
+                    <p>
+                        We may share your information with our partners and with companies who help us to operate this
+                        site and to organise events and other activities. Some of these organisations may process your
+                        information in countries outside the European Economic Area (EEA), such as the United States,
+                        where data protection laws are not the same as in the EEA. If you don’t want us to transfer your
+                        information in this way, please don’t access and use our websites.
+                    </p>
+
+                    <p>
+                        Comments, blogs and other information which you post on the site will be displayed publicly and
+                        to other users. Please be careful when disclosing personal information which may identify you or
+                        anyone else. We are not responsible for the protection or security of information which you post
+                        in public areas.
+                    </p>
+
+                    <p>
+                        We may disclose your personal information if required by law, or to protect or defend ourselves
+                        or others against illegal or harmful activities, or as part of a reorganisation or
+                        restructuring.
+                    </p>
+
+                    <h2 class="home-h2">5. Cookies</h2>
+                    <p>
+                        This site contains cookies. Cookies are small text files that are placed on your computer by
+                        websites you visit. They are widely used to make websites work, or work more efficiently, as
+                        well as
+                        to provide information to site owners. Most web browsers allow some control of most cookies
+                        through
+                        browser settings. To find out more about cookies, including how to see what cookies have been
+                        set
+                        and how to manage and delete them, visit www.aboutcookies.org or www.allaboutcookies.org.
+                    </p>
+
+                    <p>
+                        This site uses cookies that are strictly necessary to enable you to move around the site or to
+                        provide certain basic features, such as logging into secure areas.
+                        The site also uses performance cookies which collect information about how you use the site,
+                        such as
+                        how you are referred to it and how long you stay on certain pages. This information is
+                        aggregated
+                        and therefore anonymous and is only used to improve the performance of the site. Some of these
+                        performance cookies are Google Analytics web cookies. To opt out of being tracked by Google
+                        Analytics across all websites visit http://tools.google.com/dlpage/gaoptout.
+                    </p>
+
+                    <h2 class="home-h2">6. Security</h2>
+                    <p>
+                        We take steps to protect your personal information and follow procedures designed to minimise
+                        unauthorised access or disclosure of your information. However, we can’t guarantee to eliminate
+                        all
+                        risk of misuse. If you have a password for an account on this site, please keep this safe and
+                        don’t
+                        share it with anyone else. You are responsible for all activity on your account and must contact
+                        us
+                        immediately if you are aware of any unauthorised use of your password or other security breach.
+                    </p>
+
+                    <h2 class="home-h2">7. Contacting us</h2>
+                    <p>
+                        You are legally entitled to know what personal information we hold about you and how that
+                        information is processed. If you would like to know what information we hold about you, please
+                        write
+                        to the General Counsel & Company Secretary at Nesta, 1 Plough Place, London EC4A 1DE, UK. We
+                        will
+                        ask you for proof of identity and may charge a £10 fee.
+                    </p>
+
+                    <p>
+                        Nesta, a company limited by guarantee registered in England and Wales with company number 7706036
+                        and charity number 1144091.
+                    </p>
+
+                    <p>
+                        Registered as a charity in Scotland number SC042833.
+                        Registered office: 1 Plough Place, London EC4A 1DE
+                    </p>
                 </div>
             </div>
         </div>
--- www/terms-of-use.php
+++ www/terms-of-use.php
@@ -15,13 +15,70 @@ $totalOrganisations = (new \DSI\Repository\OrganisationRepository())->countAll()
     <div class="w-section section-grey">
         <div class="container-wide">
             <div class="w-row">
-                <div class="w-col w-col-6 intro-col">
-                    <h2 class="home-h2">What is the purpose of DSI4EU?</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
-                </div>
-                <div class="w-col w-col-6 intro-col">
-                    <h2 class="home-h2">What do the data visualisations below show?</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+                <div class="w-col w-col-12 intro-col">
+                    <p>
+                        This website is managed by Nesta (“Nesta”) on behalf of the European Commission and our
+                        partners, Waag Society and SUPSI. Use of the site is subject to these Terms. Our Privacy Policy
+                        explains how we use any personal information which we collect via the site. If you don’t agree
+                        with these Terms or our Privacy Policy, please don’t use this site.
+                    </p>
+                    <p>
+                        1. The information, materials and logos on this site belong to the European Commission, Nesta,
+                        our partners or licensors. Unless otherwise indicated, materials and reports are licensed for
+                        use under a Creative Commons Attribution Non-Commercial Share-A-like licence (CC-BY- NC-SA),
+                        full details at https://creativecommons.org/licenses/by-nc-sa/4.0/. We make no representation or
+                        warranty about the content, including in relation to non-infringement of intellectual property
+                        rights.
+                    </p>
+                    <p>
+                        2. You must not use this site: to post, share or transmit material which is unlawful or
+                        infringes anyone else’s rights for any commercial purpose or in any way which is abusive,
+                        defamatory or obscene or which harasses or distresses anyone or restricts or inhibits their use
+                        and enjoyment of the site or otherwise damages our reputation to transmit software viruses or
+                        code designed to interrupt, destroy or compromise the integrity of computer software, hardware
+                        or telecommunications equipment
+                    </p>
+                    <p>
+                        3. You are responsible for any comments, text, video, audio, images or other content which you
+                        post on the site and must obtain any relevant permissions or consents to post this content. You
+                        grant us a perpetual, worldwide, non-exclusive, royalty-free and fully transferable right to
+                        copy, download, distribute, transmit and adapt your content and waive your moral rights in it.
+                    </p>
+                    <p>
+                        4. We are not responsible for content posted on the site by users and third parties. The views
+                        expressed are not necessarily the views of the European Commission, Nesta or our partners. You
+                        can report any inappropriate content to us at contact@digitalsocial.eu . Although we don’t
+                        monitor all content posted on the site, we reserve the right to do so and to edit or remove
+                        content at our absolute discretion and without notice.
+                    </p>
+                    <p>
+                        5. The information on this site is subject to change from time to time and we can’t guarantee
+                        that it will always be accurate and up-to-date. We are not responsible for the content of
+                        external sites which you link to from this site.
+                    </p>
+                    <p>
+                        6. We don’t guarantee that the site will always be available or free from errors or viruses. You
+                        are responsible for installing virus-checking software to protect your hardware. As far as
+                        legally possible, we and our partners exclude all liability for any loss or damage suffered as a
+                        result of using or accessing our site, whether direct or indirect, and however arising,
+                        including any loss of data or damage caused by downloading content from our site.
+                    </p>
+                    <p>
+                        7. These Terms may be varied from time to time. If you use the site after any change has been
+                        made, you will be deemed to have accepted the change. Additional terms may apply to certain
+                        areas of this site, such as applications for grant funding or payment for events.
+                    </p>
+                    <p>
+                        8. These Terms are governed by English law and the courts of England and Wales will have
+                        jurisdiction over any dispute arising from them.
+                    </p>
+                    <p>
+                        Nesta, a company limited by guarantee registered in England and Wales with company number
+                        7706036 and charity number 1144091. Registered as a charity in Scotland number SC042833.
+                    </p>
+                    <p>
+                        Registered office: 1 Plough Place, London EC4A 1DE
+                    </p>
                 </div>
             </div>
         </div>
