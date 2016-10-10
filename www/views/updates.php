92f6e352a15d37fe7aecf90109a5d6180bc6ac58
Daniel Pettifer <danielpettifer@me.com>
2016-10-10 16:53:26 +0100

    Updated updates.html

--- www/views/updates.php
+++ www/views/updates.php
@@ -21,13 +21,25 @@ require __DIR__ . '/header.php';
     </div>
     <div class="content-directory">
         <div class="container-wide">
-            <div class="page-info-block update-card w-clearfix" data-ix="edit-update">
-                <div class="update-type">Feature</div>
-                <div class="update-date">18th July 2016</div>
-                <h4>Added ability to message other members of your project or organisation</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+            
+             <div class="page-info-block update-card w-clearfix" data-ix="edit-update">
+                <div class="update-tag update-type">Update</div>
+                <div class="update-date">10th October 2016</div>
+                <h4>DSI4EU Website update</h4>
+                <p>Digitalsocial.eu has undergone a complete overhaul.</p>
                 <a class="edit-update" href="#">Edit</a>
             </div>
+            
+            <div class="page-info-block update-card w-clearfix" data-ix="edit-update">
+                <div class="update-type">Feature</div>
+                <div class="update-date">8th August 2016</div>
+                <h4>Updates page</h4>
+                <p>Updates page has been added to keep users up to date with new features, bug fixes and updates to existing features.
+
+</p>
+                <a class="edit-update" href="#">Edit</a>
+            </div>
+<!--
             <div class="page-info-block update-card w-clearfix" data-ix="edit-update">
                 <div class="bug update-type">Bug Fix</div>
                 <div class="update-date">17th July 2016</div>
@@ -35,13 +47,9 @@ require __DIR__ . '/header.php';
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
                 <a class="edit-update" href="#">Edit</a>
             </div>
-            <div class="page-info-block update-card w-clearfix" data-ix="edit-update">
-                <div class="update-tag update-type">Update</div>
-                <div class="update-date">12th July 2016</div>
-                <h4>Social sharing is now possible on case studies as well as blog posts</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
-                <a class="edit-update" href="#">Edit</a>
-            </div>
+-->
+          
+            
         </div>
     </div>
 
