da2c2ba21184940b561e7201ffc5c32a9705c417
Alex Pandele <alecs@inoveb.co.uk>
2016-10-14 17:17:01 +0100

    Email texts improvements

--- src/email-template/welcome.php
+++ src/email-template/welcome.php
@@ -1,1 +1,4 @@
-Welcome to Digital Social<br />
+Welcome to <a href="https://<?php echo SITE_DOMAIN ?>/">DigitalSocial.eu</a>,
+<br /><br />
+After completing your registration you will be able to add a project or organisation,
+join existing projects and organisations or view funding opportunities and events.
--- src/email-template/welcomeFromApp.php
+++ src/email-template/welcomeFromApp.php
@@ -7,5 +7,7 @@ Please <a href="https://<?php echo SITE_DOMAIN ?>/reset-password">click here</a>
 to complete your registration.<br/>
 Your security code is: <b><?php echo $code ?></b>
 <br/><br/>
+For security reasons, this code expires in 24 hours. You can generate a new code after 24 hours.
+<br/><br/>
 After completing your registration you will be able to add a project or organisation,
 join existing projects and organisations or view funding opportunities and events.
