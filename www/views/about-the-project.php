05d849a017eda565c31c9fc4ecbf4cf35f6942ae
Daniel Pettifer <danielpettifer@me.com>
2016-12-07 12:44:13 +0000

    Updated about-the-project.php

--- www/views/about-the-project.php
+++ www/views/about-the-project.php
@@ -13,13 +13,11 @@ require __DIR__ . '/header.php'
                     supported digital innovation in business, as well as in fields ranging from the military to
                     espionage. But there has been much less systematic support for innovations that use digital
                     technology to address social challenges.</p>
-                <p>Across Europe a growing movement of tech entrepreneurs is developing inspiring digital solutions to
-                    social challenges. These range from social networks for those living with chronic health conditions,
-                    to online platforms for citizen participation in policymaking, to using open data to create more
-                    transparency about public spending. We call this digital social innovation (DSI). We have set up
-                    digitalsocial.eu to build a network of digital social innovation in Europe. People and projects
-                    interested in DSI can use the site to showcase their work, learn about DSI, find collaborators for
-                    projects and find information on events and funding opportunities for DSI.</p>
+                <p>Across Europe a growing movement of people developing inspiring digital solutions to social challenges. We call this digital social innovation (DSI).</p>
+                <p>These range from social networks for those living with chronic health conditions, to online platforms for citizen participation in policymaking, to using open data to create more transparency about public spending.</p>
+                <p>This site is a community for digital social innovators in Europe. People, projects and organisations interested in DSI can use the site to showcase their work, learn about DSI, find collaborators for projects and find information on events and funding opportunities.</p>
+                <p>This DSI project is a continuation of a 2012-2014 project that mapped and defined the DSI community in Europe. Alongside the platform, we will be carrying out research, recommending policy, developing a sustainability toolkit and holding various events across Europe.</p>
+                <!-- <p>Newsletter section</p> -->
                 <a class="log-in-link long read-more w-clearfix w-inline-block" data-ix="log-in-arrow"
                    href="http://digitalsocial.eu/open-data-research-and-resources">
                     <div class="login-li long menu-li readmore-li">Read more about previous research into mapping and
