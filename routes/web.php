361e4de9a66f52a84061f5ff9403b16d334cb950
Alex Pandele <alecs@inoveb.co.uk>
2018-06-16 14:16:29 +0100

    admin cand manage case studies tags

--- routes/web.php
+++ routes/web.php
@@ -588,7 +588,7 @@ class Router
     {
         $this->setLanguageFromUrl($matches);
 
-        $command = new \DSI\Controller\CaseStudyAddController();
+        $command = new \Controllers\CaseStudies\CaseStudyAddController();
         $command->exec();
     }
 
@@ -960,7 +960,7 @@ class Router
     {
         $this->setLanguageFromUrl($matches);
 
-        $command = new \DSI\Controller\CaseStudyEditController();
+        $command = new \Controllers\CaseStudies\CaseStudyEditController();
         $command->caseStudyID = $matches[3];
         $command->format = $format;
         $command->exec();
