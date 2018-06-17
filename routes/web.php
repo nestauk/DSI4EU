2ec4037c57a3e4fb8a78eddfab1b752173aca39e
Alex Pandele <alecs@inoveb.co.uk>
2018-06-17 22:32:45 +0100

    create advisory board page

--- routes/web.php
+++ routes/web.php
@@ -269,6 +269,9 @@ class Router
         } elseif ($this->pageURL === '/robots.txt') {
             $this->robotsTxt();
 
+        } elseif (preg_match('<^/' . $langHandler . 'advisory-board$>', $this->pageURL, $matches)) {
+            $this->advisoryBoard($matches);
+
         } elseif (preg_match('<^/' . $langHandler . 'explore-dsi$>', $this->pageURL, $matches)) {
             $this->staticPage($matches, 'explore-dsi.php');
 
@@ -1115,6 +1118,12 @@ class Router
         $command->format = 'txt';
         $command->view = 'robots.txt.php';
         $command->exec();
+    }
+
+    private function advisoryBoard($matches)
+    {
+        $this->setLanguageFromUrl($matches);
+        return (new \Controllers\AdvisoryBoardController())->exec();
     }
 
     private function staticPage($matches, $view)
