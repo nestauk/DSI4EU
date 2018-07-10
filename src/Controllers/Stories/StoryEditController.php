6fb31469ab895cc9ad47f6fb0e2c07c27aa9f9ac
Alex Pandele <alecs@inoveb.co.uk>
2018-07-10 10:34:55 +0100

    order authors by name

--- src/Controllers/Stories/StoryEditController.php
+++ src/Controllers/Stories/StoryEditController.php
@@ -81,7 +81,7 @@ class StoryEditController
 
         View::render(__DIR__ . '/../../Views/stories/story-edit.php', [
             'categories' => (new StoryCategoryRepo())->getAll(),
-            'writers' => (new UserRepo())->getAll(),
+            'writers' => (new UserRepo())->orderByName()->getAll(),
             'story' => $story,
             'loggedInUser' => $loggedInUser,
         ]);
