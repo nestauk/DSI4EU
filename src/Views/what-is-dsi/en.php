7aa0560147a7a3d78bdb9085eece4328e0c5696d
Alex Pandele <alecs@inoveb.co.uk>
2018-08-13 21:21:16 +0100

    Add What is DSI page content

--- src/Views/what-is-dsi/en.php
+++ src/Views/what-is-dsi/en.php
@@ -1,5 +1,6 @@
 <?php
 /** @var $urlHandler Services\URL */
+/** @var $homePageCaseStudies \DSI\Entity\CaseStudy[] */
 
 require __DIR__ . '/../header.php'
 ?>
@@ -10,107 +11,83 @@ require __DIR__ . '/../header.php'
                 <h1 class="content-h1">What is DSI?</h1>
 
                 <p class="intro">
-                    Coming soon...
+                    Digital social innovation brings together people and digital technologies to tackle social and
+                    environmental challenges.
                 </p>
                 <p class="separator">
-                    In the meantime, check out some of our <a href="<?= $urlHandler->caseStudies() ?>">case studies</a>
-                    to find inspiring stories of digital social innovation in practice.
+                    Across the world, thousands of people, projects and organisations are using digital technologies to
+                    tackle social and environmental challenges in fields ranging from
+                    <a href="http://digitalsocial.eu/cluster/1">healthcare</a>,
+                    <a href="http://digitalsocial.eu/cluster/2">education and employment</a> to
+                    <a href="http://digitalsocial.eu/cluster/3">democratic participation</a>,
+                    <a href="https://digitalsocial.eu/cluster/5/migration-and-integration">migration</a> and
+                    <a href="https://digitalsocial.eu/clusters/4">the environment</a>.
+                    These initiatives use a broad
+                    range of established and emerging technologies - including collaborative platforms, open data,
+                    citizen sensing, digital fabrication, open hardware, blockchain, machine learning, and augmented and
+                    virtual reality - to empower citizens to collaborate and deliver social impact. They have the
+                    potential to transform the way our public services operate, revitalise civic life and allow citizens
+                    to become direct participants in tackling social challenges.
                 </p>
 
-                <?php /*
-                <p class="separator">
-                    You may have heard of internet cookies... you may know all about them. To confirm, a cookie is a
-                    small file of letters and numbers stored (with your consent) on your browser or the hard drive of
-                    your computer. Cookies contain information that is transferred to your computer's hard drive.
-                </p>
-                <p>
-                    Cookies allow us to identify you (or, more accurately, your computer) when you visit our website.
-                    Knowing how often people are visiting each page of our website helps us work out how and where to
-                    improve our website.
-                </p>
-                <p>
-                    Details about the cookies we use (name, purpose and duration) are set out below.
-                </p>
-                <p class="separator">
-                    <strong>
-                        By continuing to use our website after opening this cookies policy, you consent to the use of
-                        cookies (including third party cookies) as described in this policy.
-                    </strong>
-                </p>
-                <p class="separator">
-                    <strong>
-                        sessionid
-                    </strong>
-                </p>
-                <p>
-                    This cookie is used to identify a session. It is necessary to remember the current logged in user.
-                    Its duration is until the browsing session ends (usually when the user closes the browser).
-                </p>
-                <p>
-                    <strong>
-                        permLogin
-                    </strong>
-                </p>
-                <p>
-                    This cookie is used to remember the logged in user every time they visit the website. This is active
-                    only if the user selected &ldquo;remember me&rdquo; when logging in. Its duration is one year.
-                </p>
-                <p>
-                    <strong>
-                        _ga
-                    </strong>
-                </p>
-                <p>
-                    This is a Google Analytics cookie. It helps us monitor the use and performance of our website. Its
-                    duration is two years.
-                </p>
+                <p>We call this field <strong>digital social innovation</strong> (DSI), a term which overlaps
+                    significantly with other terms including tech for good, social tech and civic tech. </p>
 
-                <p class="separator">
-                    If you want, you can block cookies by changing the settings on your browser – we have set out some
-                    links below to the 'Help' pages of some major browsers which explain how you can do this. Blocking
-                    the cookies we use may have an adverse impact upon the presentation of some or all of our website.
-                </p>
-                <p>
-                    General guidance on how to manage your cookies can be found here:
-                </p>
+                <p>DSI projects are characterised by:</p>
+                <ul>
+                    <li>the <strong>primacy of social or environmental impact</strong> over financial return;</li>
+                    <li>a dedication to <strong>openness, collaboration and citizen empowerment</strong>.</li>
+                </ul>
 
-                <p>
-                    <a target="_blank" href="http://www.aboutcookies.org">www.aboutcookies.org</a>
+                <p>The broad aims of DSI are to:</p>
+                <ul>
+                    <li>harness digital technologies to <strong>improve lives</strong> and <strong>reorient
+                            technology</strong> towards more social ends;
+                    </li>
+                    <li><strong>empower citizens</strong> to take more control over their lives, and to use their
+                        collective knowledge and skills to positive effect;
+                    </li>
+                    <li>Increase the <strong>accountability and transparency</strong> of government, business and civil
+                        society;
+                    </li>
+                    <li>foster and promote <strong>alternatives to the dominant technological and business
+                            models</strong> &mdash; alternatives which are open and collaborative rather than closed and
+                        competitive;
+                    </li>
+                    <li>use technology to create a <strong>more environmentally sustainable</strong></li>
+                </ul>
+                <p>To see examples of DSI in practice, take a look at our library of
+                    <a href="<?= $urlHandler->caseStudies() ?>">case studies</a> of innovation from across and
+                    beyond Europe and explore our <a href="https://digitalsocial.eu/viz/">data visualisation</a> of DSI
+                    projects and organisations. For a deeper look into the field and the most exciting opportunities and
+                    most pressing challenges, check out our report
+                    <a href="http://bit.ly/DSINext"><em>What next for digital social innovation?</em></a>.
                 </p>
-                <p>
-                    <a target="_blank" href="http://www.youronlinechoices.eu">www.youronlinechoices.eu</a>
-                </p>
-
-                <p class="separator">
-                    Specific guidance on how to manage cookie options for common website browsers can be found here:
-                </p>
-
-                <p>
-                    <a target="_blank"
-                       href="https://support.google.com/chrome/answer/95647?co=GENIE.Platform%3DDesktop&amp;hl=en">
-                        Chrome
-                    </a>
-                </p>
-                <p>
-                    <a target="_blank"
-                       href="https://support.mozilla.org/en-US/kb/enable-and-disable-cookies-website-preferences">
-                        Firefox
-                    </a>
-                </p>
-                <p>
-                    <a target="_blank"
-                       href="https://support.apple.com/en-gb/guide/safari/manage-cookies-and-website-data-sfri11471/mac">
-                        Safari
-                    </a>
-                </p>
-                <p>
-                    <a target="_blank"
-                       href="https://support.microsoft.com/en-gb/help/17442/windows-internet-explorer-delete-manage-cookies">
-                        Internet Explorer
-                    </a>
-                </p>
-                */ ?>
+                <p>If you&rsquo;re working on DSI, <a href="<?= $urlHandler->register() ?>">register now</a> and get
+                    your project or organisation onto the map in just a few minutes.</p>
             </div>
+        </div>
+        <div class="w-row">
+            <br>
+            <h3 class="centered title"><?php _ehtml('Case Studies') ?></h3>
+            <?php foreach ($homePageCaseStudies AS $i => $caseStudy) { ?>
+                <div class="w-col w-col-4">
+                    <a class="case-study-ind w-inline-block" data-ix="scaleimage"
+                       href="<?php echo $urlHandler->caseStudy($caseStudy) ?>">
+                        <div class="case-study-img-container">
+                            <div class="_<?php echo $i % 3 + 1 ?> case-study-img"
+                                 style="background-image: url('<?php echo \DSI\Entity\Image::CASE_STUDY_CARD_BG_URL . $caseStudy->getCardImage() ?>');"></div>
+                        </div>
+                        <h3 class="case-study-card-h3"><?php echo show_input($caseStudy->getTitle()) ?></h3>
+                        <p class="cradp"><?php echo show_input($caseStudy->getIntroCardText()) ?></p>
+                        <div class="log-in-link read-more w-clearfix" data-ix="log-in-arrow">
+                            <div class="login-li menu-li readmore-li"><?php _ehtml('Read more') ?></div>
+                            <img class="login-arrow"
+                                 src="<?php echo SITE_RELATIVE_PATH ?>/images/ios7-arrow-thin-right.png">
+                        </div>
+                    </a>
+                </div>
+            <?php } ?>
         </div>
     </div>
 
