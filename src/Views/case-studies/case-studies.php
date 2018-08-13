a93cc14792fb3a80a8214d88dd47895769e22661
Alex Pandele <alecs@inoveb.co.uk>
2018-08-13 23:23:57 +0100

    Improve design for case studies

--- src/Views/case-studies/case-studies.php
+++ src/Views/case-studies/case-studies.php
@@ -30,41 +30,53 @@ require __DIR__ . '/../header.php';
                     <?php } ?>
                 </div>
             </div>
-            <div class="case-study-row-p w-row">
-                <?php foreach ($tags as $tag) { ?>
-                    <label>
-                        <input type="checkbox" name="tagID" value="<?= $tag->getId() ?>">
-                        <?= _html($tag->{\Models\Tag::Name}) ?>
-                    </label>
-                <?php } ?>
-                <br><br>
-            </div>
-            <div class="case-study-row-p w-row">
-                <?php foreach ($caseStudies AS $i => $caseStudy) { ?>
-                    <div class="w-col w-col-4 js-case-study" data-tags='<?= $caseStudy->caseStudyTagIDs() ?>'>
-                        <a class="case-study-ind w-inline-block" data-ix="scaleimage"
-                           href="<?php echo $urlHandler->caseStudyModel($caseStudy) ?>">
-                            <div class="case-study-img-container">
-                                <div class="_<?php echo ($i % 3) + 1 ?> case-study-img"
-                                     style="background-image: url('<?php echo \DSI\Entity\Image::CASE_STUDY_CARD_BG_URL . $caseStudy->{CaseStudy::CardImage} ?>');"></div>
-                            </div>
-                            <h3 class="case-study-card-h3">
-                                <?php echo show_input($caseStudy->{CaseStudy::Title}) ?>
-                                <?php if ($userCanManageCaseStudies AND !$caseStudy->{CaseStudy::IsPublished}) {
-                                    echo ' <span style="color:red">(' . _html('Unpublished') . ')</span>';
-                                } ?>
-                            </h3>
-                            <p class="cradp">
-                                <?php echo show_input($caseStudy->{CaseStudy::IntroCardText}) ?>
-                            </p>
-                            <div class="log-in-link read-more w-clearfix" data-ix="log-in-arrow">
-                                <div class="login-li menu-li readmore-li"><?php _ehtml('Read more') ?></div>
-                                <img class="login-arrow"
-                                     src="<?php echo SITE_RELATIVE_PATH ?>/images/ios7-arrow-thin-right.png">
-                            </div>
-                        </a>
+
+            <div class="w-row">
+                <div class="filter-col-left w-col w-col-4">
+                    <div class="filter-bar info-card">
+                        <div class="w-form">
+                            <form id="email-form" name="email-form">
+                                <h3 class="sidebar-h3"><?php _ehtml('Filter case studies') ?></h3>
+                                <?php foreach ($tags as $tag) { ?>
+                                    <div class="filter-checkbox w-checkbox">
+                                        <label class="w-form-label">
+                                            <input class="w-checkbox-input" type="checkbox" name="tagID"
+                                                   value="<?= $tag->getId() ?>">
+                                            <?= _html($tag->{\Models\Tag::Name}) ?>
+                                        </label>
+                                    </div>
+                                <?php } ?>
+                            </form>
+                        </div>
                     </div>
-                <?php } ?>
+                </div>
+                <div class="w-col w-col-8 case-study-row-p w-row">
+                    <?php foreach ($caseStudies AS $i => $caseStudy) { ?>
+                        <div class="w-col w-col-6 js-case-study" data-tags='<?= $caseStudy->caseStudyTagIDs() ?>'>
+                            <a class="case-study-ind w-inline-block" data-ix="scaleimage"
+                               href="<?php echo $urlHandler->caseStudyModel($caseStudy) ?>">
+                                <div class="case-study-img-container">
+                                    <div class="_<?php echo ($i % 3) + 1 ?> case-study-img"
+                                         style="background-image: url('<?php echo \DSI\Entity\Image::CASE_STUDY_CARD_BG_URL . $caseStudy->{CaseStudy::CardImage} ?>');"></div>
+                                </div>
+                                <h3 class="case-study-card-h3">
+                                    <?php echo show_input($caseStudy->{CaseStudy::Title}) ?>
+                                    <?php if ($userCanManageCaseStudies AND !$caseStudy->{CaseStudy::IsPublished}) {
+                                        echo ' <span style="color:red">(' . _html('Unpublished') . ')</span>';
+                                    } ?>
+                                </h3>
+                                <p class="cradp">
+                                    <?php echo show_input($caseStudy->{CaseStudy::IntroCardText}) ?>
+                                </p>
+                                <div class="log-in-link read-more w-clearfix" data-ix="log-in-arrow">
+                                    <div class="login-li menu-li readmore-li"><?php _ehtml('Read more') ?></div>
+                                    <img class="login-arrow"
+                                         src="<?php echo SITE_RELATIVE_PATH ?>/images/ios7-arrow-thin-right.png">
+                                </div>
+                            </a>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
         </div>
     </div>
