ff3948130b0cb3c8e949501a1d30479f158993d4
Alex Pandele <alecs@inoveb.co.uk>
2018-08-13 19:25:42 +0100

    Add OG tags

--- src/Services/View.php
+++ src/Services/View.php
@@ -5,6 +5,7 @@ namespace Services;
 class View
 {
     static $pageTitle;
+    static $pageDescription;
 
     static function render(string $view, array $data = [])
     {
@@ -30,5 +31,15 @@ class View
     static function getPageTitleOr($default)
     {
         return self::$pageTitle ? self::$pageTitle : $default;
+    }
+
+    static function setPageDescription($description)
+    {
+        self::$pageDescription = substr($description, 0, 255);
+    }
+
+    static function getPageDescription()
+    {
+        return self::$pageDescription;
     }
 }
